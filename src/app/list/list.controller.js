0551e1b0e894a42b679afaf3a74c25a7b385856c
Jay Smith <jayvsmith@gmail.com>
2015-07-05 20:50:52 -0500

    Refactoring.

--- src/app/list/list.controller.js
+++ src/app/list/list.controller.js
@@ -6,74 +6,60 @@
     .controller('ListController', ListController);
 
   /** @ngInject */
-  function ListController($scope, motivator, todoList) {
-    var vm = this;
-    vm.motivator = selectMotivator();
-    vm.todoItems = [];
+  function ListController(motivator, todoList) {
+    var MILLIS_IN_MINUTE = 60000;
+    var MILLIS_IN_HOUR = MILLIS_IN_MINUTE * 60;
+    var MILLIS_IN_DAY = MILLIS_IN_HOUR * 24;
+    var MILLIS_IN_YEAR = MILLIS_IN_DAY * 365;
 
-    loadTodoList();
-
-    $scope.newItem = function() {
-      var newItem = $scope.newTodo.trim();
-      if (newItem) {
-        todoList.addItem(newItem);
-        loadTodoList();
-        $scope.newTodo = "";
-      }
-    };
-
-    $scope.completeItem = function(todoItem) {
-      todoList.markCompleted(todoItem.id, new Date());
-      loadTodoList();
-    };
-
-    function loadTodoList() {
-      vm.todoItems = todoList.getItems();
-      angular.forEach(vm.todoItems, calculateElapsedTime);
-    }
-
-    function calculateElapsedTime(item) {
-
-      if (item.completed && item.completed > 0) {
-        var millisInMinute = 60000;
-        var millisInHour = millisInMinute * 60;
-        var millisInDay = millisInHour * 24;
-        var millisInYear = millisInDay * 365;
-
-        var milli = new Date().getTime() - new Date(item.completed).getTime();
-
-        if (milli < millisInMinute) {
-          item.timeElapsed = "Now";
-        } else {
-          var unit = millisInMinute;
-          var scale = "minute";
-
-          if (milli > millisInYear) {
-            unit = millisInYear;
-            scale = "year";
-          } else if (milli > millisInDay) {
-            unit = millisInDay;
-            scale = "day";
-          } else if (milli > millisInHour) {
-            unit = millisInHour;
-            scale = "hour";
-          }
-
-          var elapsed = Math.floor(milli / unit);
-          item.timeElapsed = elapsed +
-                             " " +
-                             scale +
-                             ((elapsed !== 1) ? "s" : "") +
-                             " ago";
-        }
-      } else {
-        item.timeElapsed = "Never";
-      }
-    }
+    this.motivator = selectMotivator();
+    this.todoItems = todoList.getItems();
+    this.newTodo = "";
 
     function selectMotivator() {
       var motivators = motivator.getMotivators();
       return motivators[Math.floor(Math.random() * motivators.length)];
     }
+
+    this.getElapsedTime = function(lastTime) {
+      if (!lastTime || lastTime.getTime() === 0) {
+        return "Never";
+      }
+
+      var et = new Date().getTime() - new Date(lastTime).getTime();
+      var unit = 0;
+      var scale = "";
+
+      if (et > MILLIS_IN_YEAR) {
+        unit = MILLIS_IN_YEAR;
+        scale = "year";
+      } else if (et > MILLIS_IN_DAY) {
+        unit = MILLIS_IN_DAY;
+        scale = "day";
+      } else if (et > MILLIS_IN_HOUR) {
+        unit = MILLIS_IN_HOUR;
+        scale = "hour";
+      } else if (et > MILLIS_IN_MINUTE) {
+        unit = MILLIS_IN_MINUTE;
+        scale = "minute";
+      } else {
+        return "Now";
+      }
+
+      var elapsed = Math.floor(et / unit);
+      return elapsed + " " + scale + ((elapsed !== 1) ? "s" : "") + " ago";
+    };
+
+    this.newItem = function() {
+      var newItem = this.newTodo.trim();
+      if (newItem) {
+        todoList.addItem(newItem);
+        this.newTodo = "";
+      }
+    };
+
+    this.completeItem = function(todoItem) {
+      todoList.markCompleted(todoItem.id, new Date());
+    };
   }
 })();
--- src/app/list/list.controller.spec.js
+++ src/app/list/list.controller.spec.js
@@ -2,101 +2,89 @@
   'use strict';
 
   describe('list controller', function(){
-
     beforeEach(module('procrastinator'));
-
-    var scope;
-    beforeEach(inject(function($rootScope) {
-        scope = $rootScope.$new();
-    }));
+    var completionTime = new Date('2015-10-05T18:25:43.511Z');
 
     it('should return motivating exclamitory text', inject(function($controller) {
-      var vm = $controller('ListController', {$scope: scope});
+      var vm = $controller('ListController');
       expect(vm.motivator).toContain("!");
     }));
 
     it('should return a list of five items to do', inject(function($controller) {
-      var vm = $controller('ListController', {$scope: scope});
+      var vm = $controller('ListController');
       expect(angular.isArray(vm.todoItems).toBeTruthy);
       expect(vm.todoItems.length === 5).toBeTruthy();
     }));
 
     it('should calculate days since completion', inject(function($controller) {
       jasmine.clock().mockDate(new Date(2015, 9, 11));
-      var vm = $controller('ListController', {$scope: scope});
-      var item = vm.todoItems[0];
-      expect(item.timeElapsed).toEqual("5 days ago");
+      var vm = $controller('ListController');
+      expect(vm.getElapsedTime(completionTime)).toEqual("5 days ago");
     }));
 
     it('should not pluralize a singular day', inject(function($controller) {
       jasmine.clock().mockDate(new Date(2015, 9, 7));
-      var vm = $controller('ListController', {$scope: scope});
-      var item = vm.todoItems[0];
-      expect(item.timeElapsed).toEqual("1 day ago");
+      var vm = $controller('ListController');
+      expect(vm.getElapsedTime(completionTime)).toEqual("1 day ago");
     }));
 
     it('should calculate years since completion', inject(function($controller) {
       jasmine.clock().mockDate(new Date(2017, 9, 6));
-      var vm = $controller('ListController', {$scope: scope});
-      var item = vm.todoItems[0];
-      expect(item.timeElapsed).toEqual("2 years ago");
+      var vm = $controller('ListController');
+      expect(vm.getElapsedTime(completionTime)).toEqual("2 years ago");
     }));
 
     it('should calculate minutes since completion', inject(function($controller) {
       jasmine.clock().mockDate(new Date(2015, 9, 5, 14,0,0));
-      var vm = $controller('ListController', {$scope: scope});
-      var item = vm.todoItems[0];
-      expect(item.timeElapsed).toEqual("34 minutes ago");
+      var vm = $controller('ListController');
+      expect(vm.getElapsedTime(completionTime)).toEqual("34 minutes ago");
     }));
 
     it('should calculate hours since completion', inject(function($controller) {
       jasmine.clock().mockDate(new Date(2015, 9, 5, 16,0,0));
-      var vm = $controller('ListController', {$scope: scope});
-      var item = vm.todoItems[0];
-      expect(item.timeElapsed).toEqual("2 hours ago");
+      var vm = $controller('ListController');
+      expect(vm.getElapsedTime(completionTime)).toEqual("2 hours ago");
     }));
 
     it('should say "Now" for tasks completed under a second ago', inject(function($controller) {
       jasmine.clock().mockDate(new Date(2015, 9, 5, 13,26,0));
-      var vm = $controller('ListController', {$scope: scope});
-      var item = vm.todoItems[0];
-      expect(item.timeElapsed).toEqual("Now");
+      var vm = $controller('ListController');
+      expect(vm.getElapsedTime(completionTime)).toEqual("Now");
     }));
 
     it('should tell us if the task has never been completed', inject(function($controller) {
-      var vm = $controller('ListController', {$scope: scope});
-      var item = vm.todoItems[3];
-      expect(item.timeElapsed).toEqual("Never");
+      var vm = $controller('ListController');
+      expect(vm.getElapsedTime(new Date(0))).toEqual("Never");
     }));
 
     it('should add a new entry to the list service', inject(function($controller, todoList) {
-      $controller('ListController',{$scope: scope, todoList: todoList});
+      var vm = $controller('ListController',{todoList: todoList});
       spyOn(todoList, "addItem");
-      scope.newTodo = "Feed monkeys.";
-      scope.newItem();
+      vm.newTodo = "Feed monkeys.";
+      vm.newItem();
       expect(todoList.addItem).toHaveBeenCalledWith("Feed monkeys.");
     }));
 
     it('should clear the field after adding an item', inject(function($controller) {
-      $controller('ListController',{$scope: scope});
-      scope.newTodo = "Feed monkeys.";
-      scope.newItem();
-      expect(scope.newTodo).toEqual("");
+      var vm = $controller('ListController');
+      vm.newTodo = "Feed monkeys.";
+      vm.newItem();
+      expect(vm.newTodo).toEqual("");
     }));
 
     it('should not add blank items', inject(function($controller, todoList) {
-      $controller('ListController',{$scope: scope, todoList: todoList});
+      var vm = $controller('ListController',{todoList: todoList});
       spyOn(todoList, "addItem");
-      scope.newTodo = "";
-      scope.newItem();
+      vm.newTodo = "";
+      vm.newItem();
       expect(todoList.addItem).not.toHaveBeenCalled();
     }));
 
     it('should set completed time to current time', inject(function($controller, todoList) {
       jasmine.clock().mockDate(new Date(2015, 9, 11));
-      $controller('ListController',{$scope: scope, todoList: todoList});
+      var vm = $controller('ListController',{todoList: todoList});
       spyOn(todoList, "markCompleted");
-      scope.completeItem({'id': 4});
+      vm.completeItem({'id': 4});
       expect(todoList.markCompleted).toHaveBeenCalledWith(4, new Date(2015, 9, 11));
     }));
   });
